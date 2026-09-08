bd61c119a37c8588ebbf783afc0daa2f590e3a5d
LeetCodePro-ui <herhoffer.marco@gmail.com>
2026-09-08 05:44:04 +0000

    1st level encryption

--- main.py
+++ main.py
@@ -15,20 +15,63 @@ print("""
     Please enter the letter shift of your choice:
     """)
 
-while True:
-    letter_shift = input()
-    try:
-        letter_shift = int(letter_shift)
-        break
-    except ValueError:
-        print("""
-        ERROR:
-        Enter a number!
-              """)
-        continue
+#while True:
+#   letter_shift = input()
+#   try:
+#        letter_shift = int(letter_shift)
+#        break
+#    except ValueError:
+#        print("""
+#        ERROR:
+#        Enter a number!
+#              """)
+#        continue
 
-shifting = {
-        
+
+dictionary = {
+        "a":"b",
+        "b":"c",
+        "c":"d",
+        "d":"e",
+        "e":"f",
+        "f":"g",
+        "g":"h",
+        "h":"i",
+        "i":"j",
+        "j":"k",
+        "k":"l",
+        "l":"m",
+        "m":"n",
+        "n":"o",
+        "o":"p",
+        "p":"q",
+        "q":"r",
+        "r":"s",
+        "s":"t",
+        "t":"u",
+        "u":"v",
+        "v":"w",
+        "w":"x",
+        "x":"y",
+        "y":"z",
+        "z":"a"
         }
 
+text = input("Please enter your text for encryption here:\n")
+text = text.lower()
+
+encrypted_text = ""
+for i in text:
+    if i == " ":
+        encrypted_text += " "
+    elif i == "!":
+        encrypted_text += "!"
+    elif i == ".":
+        encrypted_text += "."
+    elif i == ",":
+        encrypted_text += ","
+    else:
+        encrypted_text += dictionary[i]
+
+print(f"Encrypted text:\n{encrypted_text}")
 
